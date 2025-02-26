831b60118a0c33b1d5d24518e7980b02a49168ac
ketanhirpara1 <174380902+ketanhirpara1@users.noreply.github.com>
2025-02-26 12:12:27 +0000

    add .www on GET ropc-register Complete URL

--- src/test/scala/uk/gov/hmrc/perftests/credentialManagement/requests/CmRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/credentialManagement/requests/CmRequests.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Copyright 2025 HM Revenue & Customs
 
 package uk.gov.hmrc.perftests.credentialManagement.requests
@@ -265,7 +281,8 @@ trait CmRequests extends BaseRequests {
         )
     } else {
       http("GET ropc-register Complete URL")
-        .get(s"$${saveRopcCompleteUrl}")
+//        .get(s"$${saveRopcCompleteUrl}")
+        .get(s"www./$${saveOneLogInSetupUrl}")
         .check(
           status.is(303)
         )
