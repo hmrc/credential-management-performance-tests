d8ca988a4a90fb7845d006375460e9ff9402ff2e
ketanhirpara1 <174380902+ketanhirpara1@users.noreply.github.com>
2025-03-21 09:01:14 +0000

    Fixed GET ropc-register Complete URL

--- src/test/scala/uk/gov/hmrc/perftests/credentialManagement/CmParts.scala
+++ src/test/scala/uk/gov/hmrc/perftests/credentialManagement/CmParts.scala
@@ -1,25 +1,9 @@
-/*
- * Copyright 2025 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 // Copyright 2025 HM Revenue & Customs
 
 package uk.gov.hmrc.perftests.credentialManagement
 
 import io.gatling.core.action.builder.ActionBuilder
-import uk.gov.hmrc.perftests.credentialManagement.requests.GNAPAuthRequests._
 import uk.gov.hmrc.perftests.credentialManagement.requests.{BaseRequests, CmRequests}
 
 object CmParts extends BaseRequests with CmRequests {
@@ -45,37 +29,12 @@ object CmParts extends BaseRequests with CmRequests {
         getIdentityAuthorizeVerificationURL,
         getOlfgURL,
         getOlfgAuthorizeResponseURL,
-
-
         postOneLoginStubIvPage(true),
         getOneLogInContinueURL,
         getIvAuthorizeCompleteURL,
         getIvInteractURL,
         getGuidanceHashURL,
         getGuidancePage,
-
-
-        //toActionBuilder(navigateToOneLoginSignInPageNoToken),
-        //toActionBuilder(redirectToInteractPage),
-        //toActionBuilder(redirectToSignInMethodPage),
-        //toActionBuilder(postOneLoginSignInMethodPage),
-        //toActionBuilder(getOneLoginGatewayInitialiseEndpoint),
-        //toActionBuilder(redirectToOneLoginGatewayStubPage),
-        //toActionBuilder(postOneLoginGatewayStubPage),
-        //toActionBuilder(redirectToOneLoginGatewayContinueEndpoint),
-        //toActionBuilder(navigateToCompleteFixerJourney),
-        //toActionBuilder(redirectToLocationEndpoint),
-        //toActionBuilder(redirectToOneLoginSignInPage),
-        //toActionBuilder(getOneLoginSignInPage),
-
-
-        //getManageDetailsPageURL,
-
-
-
-        //getGuidancePageURL1
-
-
         getRopcRegisterContinueUrl,
         postRopcRegisterUrl,
         getRopcRegisterCompleteUrl,
--- src/test/scala/uk/gov/hmrc/perftests/credentialManagement/requests/BaseRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/credentialManagement/requests/BaseRequests.scala
@@ -1,19 +1,3 @@
-/*
- * Copyright 2025 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 // Copyright 2025 HM Revenue & Customs
 
 package uk.gov.hmrc.perftests.credentialManagement.requests
@@ -37,13 +21,6 @@ trait BaseRequests extends ServicesConfiguration {
   def saveState: CheckBuilder[CssCheckType, NodeSelector, String] = css("input[name='state']", "value").optional.saveAs("state")
   def saveNonce: CheckBuilder[CssCheckType, NodeSelector, String] = css("input[name='nonce']", "value").optional.saveAs("nonce")
   def saveIvGuidanceHashUrl: CheckBuilder[CssCheckType, NodeSelector, String] = css("input[name='IvGuidanceHashUrl']", "value").optional.saveAs("ivGuidanceHashUrl")
-  //val feeder: Iterator[Map[String, String]] = Iterator.continually {
-  //  Map(
-  //    "randomIdentityProviderId" -> s"perf_${Random.alphanumeric.take(30).mkString}",
-  //    "randomEmail"              -> s"perf_${Random.alphanumeric.take(30).mkString}@example.com",
-  //    "randomScpCredId"          -> s"${Random.between(100000000000L, 1000000000000L).toString}"
-  //  )
-  //}
 
   val feeder: Iterator[Map[String, String]] = Iterator.continually {
     def generateRandomString: String = {
